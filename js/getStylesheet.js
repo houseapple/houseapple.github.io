15f2bb4636079f89b49fae9e9bb45839d7b96d01
Me <me@nosuchdomain.qq>
2016-01-15 21:36:17 -0500

    second commit

---
+++ js/getStylesheet.js
@@ -0,0 +1,11 @@
+function getStylesheet() {
+          var currentTime = new Date().getHours();
+          if (0 <= currentTime&&currentTime < 9) {
+           document.write("<link rel='stylesheet' href='../_generated-site/css/night.css' type='text/css'>");
+          }
+          if (18 <= currentTime&&currentTime <= 24) {
+           document.write("<link rel='stylesheet' href='../_generated-site/css/night.css' type='text/css'>");
+          }
+    }
+
+getStylesheet();
